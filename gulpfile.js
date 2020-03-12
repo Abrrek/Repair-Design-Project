66b00964750784bdc0646f31a4940322d4a5a01f
Movsar <movsar.kh.128@gmail.com>
2020-03-12 15:13:14 +0300

    fix: reloaded all files to other repository

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const {src, dest, watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const cssnano = require('gulp-cssnano');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+function bs() {
+	serveSass();
+	browserSync.init({
+			server: {
+					baseDir: "./"
+			},
+			socket: {
+				domain: "localhost:3000"
+			}
+	});
+	watch("./*.html").on('change', browserSync.reload);
+	watch("./sass/**/*.sass", serveSass);
+	watch("./sass/**/*.scss", serveSass);
+	watch("js/main.js").on('change', browserSync.reload);
+};
+
+function css() {
+	return src('./css/*.css')
+			.pipe(autoprefixer({
+				cascade: false
+			}))
+			.pipe(cssnano())
+			.pipe(rename({
+				suffix: ".min"
+			}))
+			.pipe(dest('./css'));
+};
+
+function serveSass() {
+	return src('./sass/**/*.sass', './sass/**/*.scss')
+    .pipe(sass())
+		.pipe(dest('./css'))
+		.pipe(browserSync.stream());
+};
+
+
+exports.serve = bs;
