54fe1cfa61671523a5ac0ee7569a962c985fc442
Movsar <movsar.kh.128@gmail.com>
2020-03-14 18:20:20 +0300

    feat: added arrow-up button for scrolling up

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+/*document.addEventListener("DOMContentLoaded", function(event) {
 	const modal = document.querySelector('.modal');
 	const modalBtn = document.querySelectorAll('[data-toggle=modal]');
 	const closeBtn = document.querySelector('.modal__close');
@@ -18,8 +18,45 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		if(e.keyCode === 27) {
 			modal.classList.remove('modal--visible');
 		}
-		// if(e.keyCode === 27) {
-		// 	modal.classList.remove('modal--visible');
-		// }
+	});
+}); */
+
+$(document).ready(function () {
+	var modal = $('.modal'),
+			modalBtn = $('[data-toggle=modal]'),
+			closeBtn = $('.modal__close'),
+			closeModal = $('.modal__close-block');
+
+	modalBtn.on('click', function() {
+		modal.toggleClass('modal--visible');
+	});
+
+	closeBtn.on('click', function() {
+		modal.toggleClass('modal--visible');
+	});
+
+	closeModal.on('click', function() {
+		modal.removeClass('modal--visible');
+	});
+
+	$(document).keydown(function(e) {
+		if (e.which == 27) {
+			modal.removeClass('modal--visible');
+		}
+});
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 50) {
+				$('#button-up').fadeIn();
+		} else {
+				$('#button-up').fadeOut();
+		}
+	});
+
+	$('#button-up').click(function () {
+			$('body,html').animate({
+					scrollTop: 0
+			}, 500);
+			return false;
 	});
 });
